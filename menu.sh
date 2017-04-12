a79fa9b2e5c466c7a5f8bca1bf04b62b6673cf32
Shamil Kashmeri <skashmeri@gmail.com>
2017-04-12 14:59:09 +0000

    stuff

---
+++ menu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Created by Anushan Rajakulasinga, (anushan@hubub.com)
+# SSH Proxy Menu
+# 2014/10/17
+
+menu_options=($(awk '{print $2}' /tmp/hosts))
+server_options=($(awk '{print $1}' /tmp/hosts))
+hostname=`hostname`
+
+
+MENULIST=()
+for ((i=0; i<${#menu_options[@]}; i++))
+do
+    MENULIST+=("$i" "${server_options[$i]} - ${menu_options[$i]}")
+done
+
+choice=$(whiptail --title "$hostname - BASTION GATEWAY SSH PROXY" --ok-button "Login" --menu "Choose a server" 0 0 5 "${MENULIST[@]}"  2>&1 >/dev/tty)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+        echo "Connecting to ${menu_options[$choice]} - ${server_options[$choice]} ..."
+        ssh -A ${menu_options[$choice]}
+else
+    echo "Exiting..."
+    exit
+fi
+
---
+++ sshgateway.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+TEST=/var/log/wtmp
+TEMPLATE=/etc/hosts.template
+HOSTS=/etc/hosts
+HOSTFILE=/tmp/hosts
+export AWS_CREDENTIAL_FILE=/root/.aws_cred
+#export SSH_CLIENT
+#export SSH_TTY
+#export SSH_AUTH_SOCK
+#export SSH_CONNECTION
+if [ -f $HOSTFILE ];then
+    sudo rm $HOSTFILE
+fi
+
+if [ `whoami` != 'root' ]
+then
+    sudo $0
+#    exit
+fi
+# Only update if there has been a login more recently than the last update
+#if [ $TEST -nt $HOSTS ]
+#then
+    TEMP=(`mktemp`)
+#    cat $TEMPLATE >> $TEMP
+    list_instances --headers PrivateIP,T:Name | grep internal | awk '{print $1,$2"."$4}' >> $TEMP
+#    list_instances --headers PrivateIP,T:Name | grep -v "^\(Private\|None\|IP\|T:Name\|-\)" | awk '{print $1,$2"."$4}'|sort -k 2 >> $TEMP
+    chmod 644 $TEMP
+    sudo mv $TEMP /tmp/hosts
+#fi
+
+
+#arr1=(`aws ec2 describe-instances | grep Name | awk '{ print $3 }'`)
+#arr1=(`aws ec2 describe-instances | grep Name | grep internal | awk '{if (!$3) {print "Un-named-Instance"} else {print $3}}'`)
+#arr2=(`aws ec2 describe-instances --query 'Reservations[].Instances[].[PrivateIpAddress]' --output text`)
+
